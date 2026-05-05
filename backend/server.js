5e0b48ed68efaacff08cf2c4fdc1c85dc89474ef
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-05-05 15:19:11 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 await connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
